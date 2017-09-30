033254b0dc152ed370c7888b8c889ced7b7a2ef3
hidekihhs <hideki.hhs@gmail.com>
2017-09-30 19:57:55 -0300

    canhao tiro

---
+++ Assets/Scripts/RotationCannon.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotationCannon : MonoBehaviour {
+
+	// Use this for initialization
+	/*void Start () {
+		
+	}*/
+    public int rotationOffset = 90;
+
+    // Update is called once per frame
+    void Update () {
+        if (Input.GetMouseButtonDown(0)) { 
+            Vector3 difference = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+            difference.Normalize ();
+
+            float rotZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.Euler (0f, 0f, rotZ + rotationOffset);
+        }
+    }
+}
---
+++ Assets/Scripts/Shoot.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour {
+
+    public float fireRate = 0;
+    public float Damage = 10;
+    public LayerMask notToHit;
+
+    float timeToFire = 0;
+    Transform firePoint;
+
+	// Use this for initialization
+	void Start () {
+        firePoint = transform.Find ("FirePoint");
+        if (firePoint == null)
+        {
+            Debug.LogError("No firePoint? WHAT!!!??");
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (fireRate == 0)
+        {
+            if (Input.GetButtonDown("Fire1"))
+            {
+                Shooting();
+            }
+        }else
+        {
+            if (Input.GetButton ("Fire1") && Time.time > timeToFire)
+            {
+                timeToFire = Time.time + 1 / fireRate;
+                Shooting();
+            }
+        }
+		
+	}
+    void Shooting ()
+    {
+        Debug.Log("Test");
+    }
+}
