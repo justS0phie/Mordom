bb7270d04fa781f1f6fd1b82efee74b3e5495c01
Rodrigo Silva Borges <rodrigo.sil.borges@gmail.com>
2017-09-30 14:49:59 -0300

    controlador simples de fases de jogo implementado

---
+++ Assets/Scripts/ControleDeFases.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControleDeFases : MonoBehaviour {
+
+	public FaseDeJogo fase;
+
+	public bool objetosInstanciados;
+
+	public GameObject planeta;
+
+	void Start () {
+		fase = FaseDeJogo.NONE;
+
+		objetosInstanciados = false;
+
+		planeta = null;
+	}
+
+	void MudarDeFase(FaseDeJogo novaFase) {
+
+		fase = novaFase;
+
+		switch(fase) {
+
+			case FaseDeJogo.Preparacao:
+
+			break;
+
+			case FaseDeJogo.Jogo:
+
+			break;
+		}
+	}
+	
+	void Update () {
+
+		//se os objetos a serem manipulados ainda nao estiverem instanciados, procurar na hierarquia pela tag
+		if(!objetosInstanciados)
+			ProcurarObjetos();
+		else
+			MudarDeFase(FaseDeJogo.Preparacao);
+	}
+
+	private void ProcurarObjetos() {
+
+		//aqui vem a lista de objetos a serem buscados
+		//para cada um, se for null, buscar na hierarquia pela tag
+		if(planeta == null)
+			planeta = GameObject.FindGameObjectsWithTag("Planeta")[0];
+
+		//quando todos os objetos necessarios para iniciar forem instanciados
+		//definir objetosInstanciados como true
+		if(planeta != null)
+			objetosInstanciados = true;
+	}
+}
+
+public enum FaseDeJogo {
+
+	NONE,
+	Preparacao,
+	Jogo,
+}
