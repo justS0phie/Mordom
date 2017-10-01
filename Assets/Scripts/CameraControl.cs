eb237a38a577b2acf9c621356a25be4e95ecd662
01Jonathan10 <01jonathanh10@gmail.com>
2017-10-01 14:47:48 -0300

    Ajustes dos Prefabs para adequar ao zoom

---
+++ Assets/Scripts/CameraControl.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour {
+
+	private Camera cam;
+
+	void Start()
+	{
+		cam = Camera.main;
+	}
+
+	void Update()
+	{
+		if (Input.GetKey("space"))
+		{
+			// choose the margin randomly
+			float margin = Random.Range(0.0f, 0.3f);
+			// setup the rectangle
+			cam.rect = new Rect(margin, 0.0f, 1.0f - margin * 2.0f, 1.0f);
+		}
+	}
+}
