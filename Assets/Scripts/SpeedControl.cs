9e39195a3f159c8f9061a38f53ac05cd1b5d29f6
Amanda Pereira <afp1103@gmail.com>
2017-10-03 19:53:49 -0300

    Protótipo de alteração dinâmica de velocidade

--- Assets/Scripts/SpeedControl.cs
+++ Assets/Scripts/SpeedControl.cs
@@ -5,18 +5,25 @@ using UnityEngine;
 public class SpeedControl : MonoBehaviour {
 
     float distance;
-    float speed;
+    float proportion;
 
     public GameObject planet;
+
+    void Start ()
+    {
+        float dx = planet.transform.position.x - transform.position.x;
+        float dy = planet.transform.position.y - transform.position.y;
+        float initD = new Vector2(dx, dy);
+        float initSpeed = GetComponent<Rigidbody2D>().velocity.magnitude;
+    }
 	
 	// Update is called once per frame
 	void Update () {
         float dx = planet.transform.position.x - transform.position.x;
         float dy = planet.transform.position.y - transform.position.y;
 
-        //distance = new Vector2(dx, dy).magnitude;
-        //Vector2 actual = GetComponent<Rigidbody2D>().velocity;
-        //speed = actual / distance;
-        //GetComponent<Rigidbody2D>().velocity = actual * speed;
+        distance = new Vector2(dx, dy).magnitude;
+        proportion = initD / distance;
+        GetComponent<Rigidbody2D>().velocity = initSpeed * proportion;
     }
 }
