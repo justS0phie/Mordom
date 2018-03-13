ab71ae9bcadd26f776d2098771faa7501138e936
Sarah Rodrigues Moreira <sarahrodriguesmoreira@gmail.com>
2018-03-12 22:33:56 -0300

    refazendo o menu e começando a adicionar os sliders
    Falta:
    - Relacionar as funções dos botões;
    - Colocar as sprites das naves;
    - Adicionar funções para o rgb.

---
+++ Assets/Scripts/Menu/MenuController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class MenuController : MonoBehaviour {
+	public Canvas MainMenu, LevelSelection, OptionsMenu;
+
+	public void displayMenu(){//disable other canvas//
+		MainMenu.gameObject.SetActive (true);
+		LevelSelection.gameObject.SetActive (false);
+		OptionsMenu.gameObject.SetActive (false);
+	}
+
+	public void displayOptions(){
+		MainMenu.gameObject.SetActive (false);
+		LevelSelection.gameObject.SetActive (false);
+		OptionsMenu.gameObject.SetActive (true);
+	}
+
+	public void displaySelection(){
+		MainMenu.gameObject.SetActive (false);
+		LevelSelection.gameObject.SetActive (true);
+		OptionsMenu.gameObject.SetActive (false);
+	}
+
+	public void loadSurvival(){
+		SceneManager.LoadScene ("Scenes/Scene1");
+	}
+
+	public void loadStory(){
+		SceneManager.LoadScene ("Scenes/Q_Alpha");
+	}
+
+	public void quit(){
+		Application.Quit();
+	}
+
+}
