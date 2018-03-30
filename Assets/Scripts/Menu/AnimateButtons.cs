4c65f910c1a3e187411d11611537fcabecf6e66d
Sarah Rodrigues Moreira <sarahrodriguesmoreira@gmail.com>
2018-03-30 12:31:24 -0300

    Mexendo no Menu 2

---
+++ Assets/Scripts/Menu/AnimateButtons.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimateButtons : MonoBehaviour {
+	private bool scale;
+	// Use this for initialization
+	void Start () {
+		scale = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//rotating the buttons//
+		this.GetComponent<RectTransform> ().Rotate (0, 0, 1.5f);
+		//scaling the buttons//
+		if (scale && this.GetComponent<RectTransform> ().localScale.x < 1.15f) {
+			this.GetComponent<RectTransform> ().localScale = this.GetComponent<RectTransform> ().localScale * 1.005f;
+		} 
+		else if (scale && this.GetComponent<RectTransform> ().localScale.x >= 1.15f) {
+			scale = false;
+		}
+		else if (!scale && this.GetComponent<RectTransform> ().localScale.x >= 1) {
+			this.GetComponent<RectTransform> ().localScale =this.GetComponent<RectTransform> ().localScale * 0.995f;
+		}
+		else {
+			scale = true;
+		}
+	}
+}
---
+++ Assets/Scripts/Menu/MenuController.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class MenuController : MonoBehaviour {
+	public Canvas MainMenu, LevelSelection, OptionsMenu;
+	public GameObject Objects;
+
+	void Start(){
+		PlayerPrefs.SetFloat("RedColor", 255);
+		PlayerPrefs.SetFloat("GreenColor", 255);
+		PlayerPrefs.SetFloat("BlueColor", 255);
+	}
+
+	void Update(){
+
+	}
+
+	public void displayMenu(){//disable other canvas//
+		MainMenu.gameObject.SetActive (true);
+		LevelSelection.gameObject.SetActive (false);
+		OptionsMenu.gameObject.SetActive (false);
+		Objects.SetActive (false);
+	}
+
+	public void displayOptions(){
+		MainMenu.gameObject.SetActive (false);
+		LevelSelection.gameObject.SetActive (false);
+		OptionsMenu.gameObject.SetActive (true);
+		Objects.SetActive (true);
+	}
+
+	public void displaySelection(){
+		MainMenu.gameObject.SetActive (false);
+		LevelSelection.gameObject.SetActive (true);
+		OptionsMenu.gameObject.SetActive (false);
+		Objects.SetActive (false);
+	}
+
+	public void updateRedPref(){
+		float value = OptionsMenu.GetComponent<Transform> ().GetChild (1).GetComponent<Slider> ().value;
+		PlayerPrefs.SetFloat("RedColor", value);
+	}
+
+	public void updateGreenPref(){
+		float value = OptionsMenu.GetComponent<Transform> ().GetChild (2).GetComponent<Slider> ().value;
+		PlayerPrefs.SetFloat("GreenColor", value);
+	}
+
+	public void updateBluePref(){
+		float value = OptionsMenu.GetComponent<Transform> ().GetChild (3).GetComponent<Slider> ().value;
+		PlayerPrefs.SetFloat("BlueColor", value);
+	}
+
+	public void loadSurvival(){
+		SceneManager.LoadScene ("Scenes/Scene1");
+	}
+
+	public void loadStory(){
+		SceneManager.LoadScene ("Scenes/Q_Alpha");
+	}
+
+	public void quit(){
+		Application.Quit();
+	}
+
+}
