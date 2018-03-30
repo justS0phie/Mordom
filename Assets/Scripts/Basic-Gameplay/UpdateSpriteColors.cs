41b48da4160387de3c0d5ce1fcd00a5936b31888
Sarah Rodrigues Moreira <sarahrodriguesmoreira@gmail.com>
2018-03-30 13:02:44 -0300

    Mexendo no Menu 3

---
+++ Assets/Scripts/Basic-Gameplay/UpdateSpriteColors.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpdateSpriteColors : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float red = PlayerPrefs.GetFloat("RedColor");
+		float green = PlayerPrefs.GetFloat("GreenColor");
+		float blue = PlayerPrefs.GetFloat("BlueColor");
+		this.GetComponent<SpriteRenderer> ().color = new Color (red/255, green/255, blue/255, 255);
+	}
+}
