14cf9a4e20c97df63696d9da11d1006b9a2ee0bf
Kaelgeass <Kaelcoke@gmail.com>
2018-04-07 18:26:40 -0300

    Pre-set de Contraste
    Botões para ajuste de Contraste criados

---
+++ Assets/Scripts/Menu/AjusteContraste.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AjusteContraste : MonoBehaviour {
+	public Slider SliderHue;
+	public Slider SliderSaturacao;
+	public Slider SliderBrilho;
+
+	public float hue;
+	public float saturacao;
+	public float brilho;
+
+	// Use this for initialization
+	public void SetContrast () {
+		SliderHue.value = hue;
+		SliderSaturacao.value = saturacao;
+		SliderBrilho.value = brilho; 
+	}
+}
